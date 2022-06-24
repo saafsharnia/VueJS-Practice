805640f46b2308c9c03d0bc182e9c8e84088f037
saeedeh <saeedeh.afsharnia@tchibo.de>
2022-06-24 18:07:00 +0200

    Add more practice, vue-cli

--- udemy/prj-monster-01-starting-setup/app.js
+++ udemy/prj-monster-01-starting-setup/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
+      logMessages: []
     };
   },
   computed: {
@@ -51,21 +52,25 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
     attackMonster() {
       this.currentRound ++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue)
     },
     specialAttackMonster() {
       this.currentRound ++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     healPlayer() {
@@ -76,10 +81,18 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue)
       this.attackPlayer(); 
     },
     surrender() {
       this.winner = 'monster';
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      })
     }
   }
 });
